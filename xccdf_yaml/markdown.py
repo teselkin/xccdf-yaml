d437f4fe8b07f683c1ebdcdd7a1a027e8fbf9e18
Dmitry Teselkin <dteselkin@mirantis.com>
2018-09-25 13:58:00 +0300

    Select only <p> elements from markdown text

--- xccdf_yaml/markdown.py
+++ xccdf_yaml/markdown.py
@@ -6,8 +6,9 @@ class MarkdownHtml(object):
     def __init__(self, text):
         self.text = text
         self.html = etree.HTML(markdown.markdown(self.text.rstrip()))
+        self.ns = 'xhtml'
         self.nsmap = {'xhtml': 'http://www.w3.org/1999/xhtml'}
-        self.html = self.set_default_ns(self.html, default_ns='xhtml')
+        self.html = self.set_default_ns(self.html, default_ns=self.ns)
 
     def set_default_ns(self, element, default_ns=None):
         e = etree.Element(etree.QName(self.nsmap[default_ns], element.tag),
@@ -21,4 +22,13 @@ class MarkdownHtml(object):
         return e
 
     def __str__(self):
-        return etree.tostring(self.html).decode()
+        elements = self.html.xpath(
+            '/{ns}:html/{ns}:body/{ns}:p'.format(ns=self.ns),
+            namespaces=self.nsmap)
+        paragraphs = ['',]
+        for x in elements:
+            text = etree.tostring(x, pretty_print=True).decode().strip()
+            if text:
+                paragraphs.append(text)
+        paragraphs.append('')
+        return '\n'.join(paragraphs)
